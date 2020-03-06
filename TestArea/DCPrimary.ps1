21991fd18cdff34fab924dfe97421c985ec342c3
Levon Gray <levon.gray@civica.com.au>
2020-03-06 13:13:16 +1100

    initial upload

---
+++ TestArea/DCPrimary.ps1
@@ -0,0 +1,18 @@
+$DomainName = $args[0]
+$Netbios = $args[1]
+$DAPassword = $args[2]
+
+
+Add-WindowsFeature -Name "ad-domain-services, dns, gpmc" -IncludeAllSubFeature -IncludeManagementtools
+Install-ADDSForest -CreateDnsDelegation:$false`
+ -DatabasePath "C:\Windows\NTDS"`
+ -DomainMode "WinThreshold"`
+ -DomainName $DomainName`
+ -DomainNetbiosName $Netbios`
+ -SafeModeAdministratorPassword $DAPassword`
+ -ForestMode "WinThreshold"`
+ -InstallDns:$true`
+ -LogPath "C:\Windows\NTDS"`
+ -NoRebootOnCompletion:$false`
+ -SysvolPath "C:\Windows\SYSVOL"`
+ -Force:$true
