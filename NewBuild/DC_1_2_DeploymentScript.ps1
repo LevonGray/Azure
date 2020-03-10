1b0937f15b6cd622c1723e32e9774e576908fbdc
Levon Gray <levon.gray@civica.com.au>
2020-03-10 14:31:37 +1100

    initial commit. Change variables to suit.

---
+++ NewBuild/DC_1_2_DeploymentScript.ps1
@@ -0,0 +1,32 @@
+
+## adjust the following variables to suit
+$custcode = "CUST"
+$VNetName = "VNET"
+$Username = "cspadmin"
+
+## Get VNET and find avilable IPs
+$VNET = Get-AzVirtualNetwork -Name $VNetName
+$IPAddresses = (Test-AzPrivateIPAddressAvailability -VirtualNetwork $VNET -IPAddress ($VNET.Subnets.addressprefix.Split("/")[0])).AvailableIPAddresses
+$DC1IP = [string]$Ipaddresses[0]
+$DC2IP = [string]$Ipaddresses[1]
+
+## Create a new resource group for the domain controllers in the same region as the VNET created.
+$RG = New-AzResourceGroup -Name "RG-$($custcode)-DomainControllers" -Location $VNET.Location
+
+## Deploy the template for standard Domain Controllers you will be prompted to set the local admin password
+new-AzResourceGroupDeployment -TemplateUri "https://raw.githubusercontent.com/LevonGray/Azure/test-deployment/NewBuild/DC_1_2_Deployment_noNSG.json" `
+ -custcode $Custcode `
+ -ResourceGroupName $RG.ResourceGroupName `
+ -subnetname $VNET.Subnets.name `
+ -virtualnetworkid $VNET.id `
+ -dc1ipaddress $DC1IP `
+ -dc2ipaddress $DC2IP `
+ -adminusername $Username
+
+## Set the IP Addresses of the DNS Servers as the DNS on the VNET
+ $array = @($DC1IP,$DC2IP)
+ $object = new-object -type PSObject -Property @{"DnsServers" = $array}
+ $VNET.DhcpOptions = $object
+ $VNET | Set-AzVirtualNetwork
+
+## Now ready to configure the VMs as Domain Controllers
